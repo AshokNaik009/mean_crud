634faa1caf41c681304b0ef9fa2a6c8c13f2647a
Ashok Naik <naikashok08@gmail.com>
2019-10-22 14:29:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
 
     //DatabaseLink
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://ashok:fcbarcelona@cluster0-defy8.mongodb.net/test?retryWrites=true&w=majority',
+   //Add the link here
     //PORT on the Machine
     PORT : 3000,
 
